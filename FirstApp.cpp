5492e2ae65f2d424d8f6f1b6eb43d2ad3a83155a
venkatakhil1000 <venkatakhil1000@gmail.com>
2021-06-22 15:23:01 +0530

    first commit

---
+++ FirstApp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+     float annualSalary;
+     cin>>annualSalary;
+     float monthlySalary=annualSalary/12;
+
+     cout<<monthlySalary;
+}
